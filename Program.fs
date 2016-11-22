2a4c0a7d160f22fce2d91553cb05682b29a36c11
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-11-22 07:19:36 -0500

    Parsing floats and abstracting parsers'

--- Program.fs
+++ Program.fs
@@ -5,7 +5,8 @@ open FParsec
 type UserState = unit 
 type Parser<'t> = Parser<'t, UserState>
 
-let test p str =
+// Parsing a single float
+let test (p:Parser<_,_>) str =
     match run p str with 
     | Success(result, _, _) -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
@@ -18,20 +19,40 @@ test pfloat "42" //Nota Bene: Integers are treated as floats
 (*This lets us know of a word exists in a string collection. 
 if it does, Success is returned, otherwise Failure showing 
 us exactly what failed (in our case, "input" fails since 
-the Parser is expecting a 'Result of "test")
+the Parser is expecting a 'Result of "test"). This also parses
+as single float
 *) 
 let str s = pstring s
 let floatBetweenBrackets(s:string) = str "[" >>. pfloat .>> str "]"
-let p : Parser<_> = pstring "test"
+let p : Parser<_> = pstring "1.25"
+let q : Parser<_> = pstring "test"
 test p "input"
-test p "test"
+test p "1.25"
+test q "test"
 
 (* This will return an array of string elements showing the elements
-immediately preceeding and following then "." 
+immediately preceeding and following then ".", which are 1 and 2
+respectively:
 *)
 let x : Parser<_> = pstring "Object: 1.23"
 let run x (s:string) : string [] = s.Split('.')
 run x "1.23" |> printfn "%A"
+
+//Parsing a float between brackets
+let floatBetweenBrackets2 = str "[" >>. pfloat .>> str "]"
+test floatBetweenBrackets2 "[2.0]"
+test floatBetweenBrackets2 "[]"
+test floatBetweenBrackets2 "[2.0"
+
+//Abstracting parsers
+let betweenStrings s1 s2 px = str s1 >>. px .>> str s2 
+let floatBetweenBrackets3 = pfloat |> betweenStrings "[" "]"
+let floatBetweenDblBrackets = pfloat |> betweenStrings "[[" "]]"
+
+test floatBetweenBrackets3 "[1.0]"
+test floatBetweenBrackets3 "[]"
+test floatBetweenBrackets3 "[1.0"
+test floatBetweenDblBrackets "[[1.0]]"
 
 
 [<EntryPoint>]
