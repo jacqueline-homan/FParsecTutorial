3004e8d2901229b23c6a474ca80934cef7873f1a
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-11-29 10:22:43 -0500

    attempted to do JSON parser part of tutorial

--- Program.fs
+++ Program.fs
@@ -249,11 +249,72 @@ let m : Parser<_> = pstring "test"
 run m "test" |> printfn "%A"
 (*If we need to have a generic type without having to 
 worry about compiler errors for value restrictions,
-we use then F# "escape hatch" - the [<GeneralizableValue>] attribute.*) 
+we use the F# "escape hatch" - the [<GeneralizableValue>] attribute.*) 
 [<GeneralizableValue>]
 let h<'T, 'u> = pstring "test"
 run h "test" |> printfn "%A"
 printfn "\n"
+
+//Writing a JSON parser
+type Json = 
+    |JString of string
+    | JNumber of float
+    | JBool   of bool
+    | JNull
+    | JList   of Json list
+    | JObject of Map<string, Json>
+
+let jstring<'T,'u> = stringReturn " "JString
+let jnull<'T, 'u> = stringReturn "null" JNull
+let jool<'T, 'u> =  (stringReturn "true"  (JBool true))
+                    <|> (stringReturn "false" (JBool false))
+let jnumber<'T, 'u> = pfloat |>> JNumber
+let stringLiteral4<'T, 'u> =
+    let escape =  anyOf "\"\\/bfnrt"
+                  |>> function
+                      | 'b' -> "\b"
+                      | 'f' -> "\u000C"
+                      | 'n' -> "\n"
+                      | 'r' -> "\r"
+                      | 't' -> "\t"
+                      | c   -> string c // every other char is mapped to itself
+
+    let unicodeEscape =
+        /// converts a hex char ([0-9a-fA-F]) to its integer number (0-15)
+        let hex2int c = (int c &&& 15) + (int c >>> 6)*9
+
+        str "u" >>. pipe4 hex hex hex hex (fun h3 h2 h1 h0 ->
+            (hex2int h3)*4096 + (hex2int h2)*256 + (hex2int h1)*16 + hex2int h0
+            |> char |> string
+        )
+
+    let escapedCharSnippet = str "\\" >>. (escape <|> unicodeEscape)
+    let normalCharSnippet  = manySatisfy (fun c -> c <> '"' && c <> '\\')
+
+    between (str "\"") (str "\"")
+            (stringsSepBy normalCharSnippet escapedCharSnippet)
+
+let jvalue, jvalueRef = createParserForwardedToRef<Json, unit>()
+
+let listBetweenStrings sOpen sClose pElement f =
+    between (str sOpen) (str sClose)
+            (ws >>. sepBy (pElement .>> ws) (str "," >>. ws) |>> f)
+
+let jlist   = listBetweenStrings "[" "]" jvalue JList
+
+let keyValue = stringLiteral .>>. (ws >>. str ":" >>. ws >>. jvalue)
+
+let jobject = listBetweenStrings "{" "}" keyValue (Map.ofList >> JObject)
+
+do jvalueRef := choice [jobject
+                        jlist
+                        jstring
+                        jnumber
+                        jtrue
+                        jfalse
+                        jnull]
+let json = ws >>. jvalue .>> ws .>> eof
+
 
 
 [<EntryPoint>]
